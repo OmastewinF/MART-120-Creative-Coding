4e04a3c2833ee0f629268673034b1d3c4fa2f616
OmastewinF <omaste-win.foster@umconnect.umt.edu>
2022-10-31 18:21:26 -0600

    week 9

---
+++ Homework 9/sketch.js
@@ -0,0 +1,45 @@
+
+
+
+
+function setup()
+{
+  createCanvas (500, 600);
+}
+function draw ()
+{
+  background (120, 100, 220);
+  text('by, omastewin foster', 50, 60);
+  fill(255, 204, 0);
+  circle(30, 30 , 30);
+  fill('magenta');
+   square(210, 535, 60);
+   fill('magenta');
+   square(310, 535, 60);
+   fill(225, 204, 0);
+   rect(320, 355, 25, 180);
+   fill(225, 204, 0);
+   rect(245, 355, 25, 180);
+fill(0,0, 225);
+rect(235, 280, 125, 180);
+fill (255, 204, 0);
+circle(300, 225, 125);
+fill('magenta');
+triangle(400, 200, 135, 245, 290, 90);
+fill(20, 75, 200);
+rect(220,300,20,120);
+fill(20, 75, 200);
+rect(350,300,20,120);
+fill(255, 204, 0);
+circle( 360, 410, 30);
+fill(255, 204, 0);
+circle(230, 410, 30);
+line(250, 250, 350, 250);
+circle(255, 200, 20);
+circle(325, 195, 20);
+line(225, 190, 290, 180);
+line(350, 190, 300, 180);
+triangle(400, 230, 290, 245, 255, 220);
+point(255, 200);
+point(325, 195);
+}
